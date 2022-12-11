f266b73d847cb408a482eb13b20ef425c86ee262
Huxley <huxdavi@gmail.com>
2022-12-10 23:51:13 -0300

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <!--=============== BOXICONS ===============-->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
+
+        <!--=============== CSS ===============-->
+        <link rel="stylesheet" href="assets/css/styles.css">
+
+        <title>Responsive website delivery</title>
+    </head>
+    <body>
+        <!--=============== HEADER ===============-->
+        <header class="header" id="header">
+            
+        </header>
+
+        <main class="main">
+            <!--=============== HOME ===============-->
+            <section class="home section" id="home">
+                
+            </section>
+
+            <!--=============== ABOUT ===============-->
+            <section class="about section container" id="about">
+                
+            </section>
+
+            <!--=============== SECURITY ===============-->
+            <section class="security section container">
+                
+            </section>
+
+            <!--=============== SERVICES ===============-->
+            <section class="services section container" id="services">
+                
+            </section>
+
+            <!--=============== APP ===============-->
+            <section class="app section container">
+                
+            </section>
+
+            <!--=============== CONTACT US ===============-->
+            <section class="contact section container" id="contact">
+                
+        </main>
+
+        <!--=============== FOOTER ===============-->
+        <footer class="footer section">
+            <
+        </footer>
+
+        <!--=============== SCROLL UP ===============-->
+        <a href="#" class="scrollup" id="scroll-up">
+            <i class='bx bx-up-arrow-alt scrollup__icon'></i>
+        </a>
+
+        <!--=============== MAIN JS ===============-->
+        <script src="assets/js/main.js"></script>
+    </body>
+</html>
